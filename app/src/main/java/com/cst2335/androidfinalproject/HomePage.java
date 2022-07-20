ea094abb8fac64af7483f705dfd08f45ddd9f252
AbdulMajeed <chowdhury4554@gmail.com>
2022-07-19 21:30:57 -0400

    test commit 2

--- app/src/main/java/com/cst2335/androidfinalproject/HomePage.java
+++ app/src/main/java/com/cst2335/androidfinalproject/HomePage.java
@@ -11,5 +11,6 @@ public class HomePage extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // comment
+        // another comment
     }
 }
