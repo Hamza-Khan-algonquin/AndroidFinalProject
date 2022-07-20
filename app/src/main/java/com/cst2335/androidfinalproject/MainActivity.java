d38a48dc581452e160f96f5391f81516916a5301
Caner <105438805+altu0013@users.noreply.github.com>
2022-07-19 21:39:54 -0400

    Created favourites page with listview

--- app/src/main/java/com/cst2335/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/cst2335/androidfinalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.cst2335.androidfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button2).setOnClickListener((click)->
+        {
+            Intent goToFavourites =new Intent(MainActivity.this, favouritesActivity.class);
+            startActivity(goToFavourites);
+        });
     }
 }
