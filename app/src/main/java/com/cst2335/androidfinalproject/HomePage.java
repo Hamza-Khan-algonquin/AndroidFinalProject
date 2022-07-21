d6fd1a453ad80d2cc7e1151784f094f6d4992c0d
Hamza-Khan-algoqnuin <khan0440@algonquinlive.com>
2022-07-20 20:26:35 -0400

    made it possible for any activity to work

--- app/src/main/java/com/cst2335/androidfinalproject/HomePage.java
+++ app/src/main/java/com/cst2335/androidfinalproject/HomePage.java
@@ -10,7 +10,6 @@ public class HomePage extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // comment
-        // another comments
+       //testing
     }
 }
